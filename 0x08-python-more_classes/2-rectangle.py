6e4542e04cb956c093d9eb1c2549a7c3cf643ad1
kodkurei <kureiallan47@gmail.com>
2023-06-06 17:05:24 +0300

    updated file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -2,11 +2,12 @@
 # 2-rectangle.py
 """Defines a Rectangle class."""
 
+
 class Rectangle:
     """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-    """Initialize a new Rectangle.
+        """Initialize a new Rectangle.
     Args:
     width (int): The width of the new rectangle.
     height (int): The height of the new rectangle.
@@ -39,12 +40,13 @@ class Rectangle:
             if value < 0:
                 raise ValueError("height must be >= 0")
             self.__height = value
+
             def area(self):
                 """Return the area of the Rectangle."""
                 return (self.__width * self.__height)
+
             def perimeter(self):
                 """Return the perimeter of the Rectangle."""
                 if self.__width == 0 or self.__height == 0:
                     return (0)
                 return ((self.__width * 2) + (self.__height * 2))
-
