3ed67eadc6dc0a61e73ffa8cb84423640e190347
kodkurei <kureiallan47@gmail.com>
2023-06-06 21:59:31 +0300

    updated file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -2,11 +2,12 @@
 # 3-rectangle.py
 """Defines a Rectangle class."""
 
+
 class Rectangle:
     """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-    """Initialize a new Rectangle.
+        """Initialize a new Rectangle.
     Args:
     width (int): The width of the new rectangle.
     height (int): The height of the new rectangle.
@@ -51,7 +52,7 @@ class Rectangle:
                 return ((self.__width * 2) + (self.__height * 2))
 
             def __str__(self):
-            """Return the printable representation of the Rectangle.
+                """Return the printable representation of the Rectangle.
             Represents the rectangle with the # character.
             """
             if self.__width == 0 or self.__height == 0:
@@ -62,4 +63,3 @@ class Rectangle:
                 if i != self.__height - 1:
                     rect.append("\n")
                     return ("".join(rect))
-
