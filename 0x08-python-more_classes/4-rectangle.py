33e4248916ca685bec8e9a76026d32dc20dbdb22
kodkurei <kureiallan47@gmail.com>
2023-06-07 01:39:16 +0300

    latest file

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -53,12 +53,13 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-    """Return the printable representation of the Rectangle.
+        """Return the printable representation of the Rectangle.
 
-    Represents the rectangle with the # character.
-    """
+        Represents the rectangle with the # character.
+        """
         if self.__width == 0 or self.__height == 0:
             return ("")
+
         rect = []
         for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
